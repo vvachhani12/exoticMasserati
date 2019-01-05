c4c6ee4044f78516ec93b6f10c0338fe19882810
surmurat <mrtsrnlr@gmail.com>
2019-01-04 19:33:09 -0500

    Added functions to retrieve make and models from the database.

--- config/orm.js
+++ config/orm.js
@@ -2,74 +2,102 @@ var connection = require("../config/connection.js");
 
 var orm = {
 
-//// this will select all available cars 
-selectAllCars: function(cb) {
-var queryString = "SELECT * FROM cars";
-    connection.query(queryString, function(err, result) {
+  //// this will select all available cars 
+  selectAllCars: function (cb) {
+    var queryString = "SELECT * FROM cars";
+    connection.query(queryString, function (err, result) {
       if (err) throw err;
       cb(result)
       console.log(result);
     });
   },
-//// this will select cars of a certain make 
+  //// this will select cars of a certain make 
 
-selectWhere: function(carMake, cb) {
+  selectWhere: function (carMake, cb) {
     var queryString = "SELECT * FROM cars WHERE car_Make = ?";
-    connection.query(queryString,carMake, 
-        function(err, result) {
+    connection.query(queryString, carMake,
+      function (err, result) {
 
-      if (err) throw err;
-      cb(result)
-      console.log(result);
-    });
+        if (err) throw err;
+        cb(result)
+        console.log(result);
+      });
   },
 
-// adding a car to the database
-  createCar: function(carYear, carMake, carModel, transmission, startDate, endDAte, miles, cb) {
+  // adding a car to the database
+  createCar: function (carYear, carMake, carModel, transmission, startDate, endDAte, miles, cb) {
     var queryString = "INSERT INTO cars SET ?";
     console.log(queryString);
     connection.query(queryString,
-        
-        {
-          car_Year: carYear,
-          car_Make: carMake,
-          car_Model: carModel, 
-          transmission_Type: transmission, 
-          start_DA: startDate, 
-          end_DA: endDAte, 
-          car_Miles: miles
-        },
-        function(err, result) {
-      if (err) throw err;
-      cb(result)
-      console.log(result);
-    });
+
+      {
+        car_Year: carYear,
+        car_Make: carMake,
+        car_Model: carModel,
+        transmission_Type: transmission,
+        start_DA: startDate,
+        end_DA: endDAte,
+        car_Miles: miles
+      },
+      function (err, result) {
+        if (err) throw err;
+        cb(result)
+        console.log(result);
+      });
   },
 
   //updating the availabilty of a car 
-//   all submissions need to be in this format '2018-01-01'
+  //   all submissions need to be in this format '2018-01-01'
 
-  updateAvail: function(startDate, endDate, thisID, cb) {
+  updateAvail: function (startDate, endDate, thisID, cb) {
     var queryString = "UPDATE cars SET ? WHERE ?";
     console.log(queryString);
     connection.query(queryString,
-        
-        [{ 
-            start_DA: startDate,
-            end_DA: endDate,
-          }, 
-         {
-            id: thisID,
-         }
-        ],
-        
-        
-    function(err, result) {
+
+      [{
+        start_DA: startDate,
+        end_DA: endDate,
+      },
+      {
+        id: thisID,
+      }
+      ],
+
+
+      function (err, result) {
+        if (err) throw err;
+        cb(result)
+        console.log(result);
+      });
+  },
+
+  getAllMake: function (cb) {
+    var queryString = "SELECT DISTINCT car_Make FROM cars ORDER BY car_Make ASC";
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
       if (err) throw err;
-      cb(result)
+      cb(result);
       console.log(result);
     });
   },
+
+  getAllModel: function (carMake, cb) {
+    var queryString = "SELECT DISTINCT car_Model FROM cars WHERE ?";
+    console.log(queryString);
+    connection.query(queryString,
+      [
+        
+        {
+          car_Make: carMake
+        }
+      ],
+      function (err, result) {
+        if (err) throw err;
+        cb(result);
+        console.log(result);
+      });
+
+  }
 }
 
 
